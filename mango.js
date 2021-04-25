23e4682e066c0bb523b10d865d86c2f6c79c5489
parthivi9 <76389790+parthivi9@users.noreply.github.com>
2021-04-25 17:04:11 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,14 @@
+class mango{
+    constructor(x,y,r)
+    {
+        var options ={
+            isStatic:true,
+            restitution:0
+            friction:1,
+        }
+        this.x=x
+        this.y=y
+        this.r=r
+
+    }
+}
